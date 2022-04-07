c70bb27108a682f8655f088a31282389341e18e5
clrali <clara4695@gmail.com>
2022-04-06 22:30:34 -0400

    added new features and updates css

--- src/index.js
+++ src/index.js
@@ -61,17 +61,32 @@ function Square(props) {
     }
 
     handleClick(i) {
+      const coordinates = [
+        [1,1],
+        [1,2],
+        [1,3],
+        [2,1],
+        [2,2],
+        [2,3],
+        [3,1],
+        [3,2],
+        [3,3]
+      ];
       const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
-      const squares = current.squares.slice(); 
+      const squares = current.squares.slice();
+
       if (calculateWinner(squares) || squares[i]){
           return;
       }
-      squares[i] = this.state.xIsNext ? 'X' : 'O'; 
+
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+
       this.setState({
         history: history.concat([
           {
-          squares: squares
+          squares: squares,
+          coordinates: coordinates[i]
           }
         ]),       
         stepNumber: history.length,   
@@ -93,12 +108,13 @@ function Square(props) {
 
       const moves = history.map((step, move) => {
         const desc = move ? 
-        "Go to move " + move : 
+        "Go to move " + move + " at position (" + history[move].coordinates + ")": 
         "Restart game";
         return (
           <ul key={move}>
-            <button onClick={() =>
-            this.jumpTo(move)}>{desc}</button>
+            <button onClick={() => this.jumpTo(move)}>
+            {move == this.state.stepNumber ? <b>{desc}</b> : desc}
+            </button>
           </ul>
         );
       });
@@ -138,6 +154,7 @@ function Square(props) {
       [0, 4, 8],
       [2, 4, 6],
     ];
+
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
